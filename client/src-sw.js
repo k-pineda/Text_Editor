817661d591abcefccc9df6832051333aabd1d115
k-pineda <karenp9310@gmail.com>
2023-09-11 10:46:46 -0400

    implented asset caching & offline fallback strategy for index.html

--- client/src-sw.js
+++ client/src-sw.js
@@ -6,6 +6,11 @@ const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
 precacheAndRoute(self.__WB_MANIFEST);
+// Define a cache name for assets
+const assetCacheName = 'assets-cache';
+
+// Define an array of file extensions for assets you want to cache
+const assetFileExtensions = ['css', 'js', 'png', 'jpg', 'gif', 'svg'];
 
 const pageCache = new CacheFirst({
   cacheName: 'page-cache',
@@ -24,7 +29,38 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
+offlineFallback({
+  route: '/index.html',
+  strategy: new CacheFirst({
+    cacheName: 'offline-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  }),
+});
+
+
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
-registerRoute();
+// Register a route for caching assets
+registerRoute(
+  ({ request }) => {
+    // Check if the request's file extension matches the allowed extensions
+    const url = new URL(request.url);
+    return assetFileExtensions.some(ext => url.pathname.endsWith(`.${ext}`));
+  },
+  new CacheFirst({
+    cacheName: assetCacheName,
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+      new ExpirationPlugin({
+        maxAgeSeconds: 7 * 24 * 60 * 60, // Cache assets for 7 days
+      }),
+    ],
+  })
+);
